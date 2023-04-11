4aafa12ae0ea63bfde58e2359c15c74079646316
Antoine BLEVIN <ablevin@e2r5p10.clusters.42paris.fr>
2023-04-11 12:44:01 +0200

    Added enum and struct for token

--- inc/minishell.h
+++ inc/minishell.h
@@ -1,12 +1,34 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+//INCLUDES
 # include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
 # include <stdlib.h>
 # include "libft.h"
 
-void prompt(void);
+//ENUMS
+typedef	enum e_token
+{
+	WORD;
+	PIPE;
+	FILE;
+	CMD;
+	IN_REDIR;
+	OUT_REDIR;
+	IN_HEREDOC;
+	OUT_HEREDOC;
+}	t_token;
+
+//STRUCTS
+typedef	struct s_token
+{
+	char	*str;
+	int		token;
+}
+
+//FUNCTIONS
+void	prompt(void);
 
 #endif
