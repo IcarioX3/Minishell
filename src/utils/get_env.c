50fc46340b4d2daa5dc493b26436b51d1e6a4622
Franck GOJARD <frgojard@e2r4p12.clusters.42paris.fr>
2023-05-10 11:07:12 +0200

    add getenv

---
+++ src/utils/get_env.c
@@ -0,0 +1,16 @@
+#include "minishell.h"
+
+char	*ft_getenv(char	*name, char **env)
+{
+    int i;
+    int len;
+	i = 0;
+	len = strlen(name);
+    while (env[i] != NULL) 
+	{
+        if ((strncmp(name, env[i], len) == 0) && (env[i][len] == '='))
+            return env[i] + len + 1;
+		i++;
+    }
+    return (NULL);
+}
